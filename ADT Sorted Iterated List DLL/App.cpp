ef07a8ae8955e9e16e76b3474b91f47ad992aa49
bianca-iorgoaea <49571709+bianca-iorgoaea@users.noreply.github.com>
2021-05-01 18:38:32 +0300

    Create App.cpp

---
+++ ADT Sorted Iterated List DLL/App.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "ShortTest.h"
+#include "ExtendedTest.h"
+#include "SortedIteratedList.h"
+
+int main(){
+    testAll();
+    testAllExtended();
+    std::cout<<"Finished IL Tests!"<<std::endl;
+	system("pause");
+}
